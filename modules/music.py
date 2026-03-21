7aba6647d933fa24be06a449b581bfba7f60a545
Jeet Bubna <bubnajeet@gmail.com>
2026-03-21 18:02:38 +0530

    Music: Implemented Thread Events to contorl Playing

--- modules/music.py
+++ modules/music.py
@@ -1,26 +1,29 @@
-from queue import PriorityQueue
 from classes import Packets
 import logging
+import random
 
 logger = logging.getLogger(__name__)
 
-def main(text:str, queues):
-    main_queue = queues["queue"]
-    listening_queue = queues["listening_queue"]
+def main(queues, event):
+
     isRunning = True
-    listening_queue.put((1,Packets(content={
-        "started":True,
-        "function": "music",
-        "end":False
-    },  queue=listening_queue)))
+
     while isRunning:
+        event.wait()
+        logger.info("GOT IT! Running program...")
+        main_queue = queues["queue"]
+        listening_queue = queues["listening_queue"]
+        listening_queue.put((random.random() + 1,Packets(content={
+            "started":True,
+            "function": "music",
+            "end":False
+        },  queue=listening_queue)))
         try:
             priority, packet = main_queue.get()
             content = packet._content
 
             if content == "end":
                 logger.info("Recieved END. Returning....") 
-                # Do something... return 1 if succesful
                 listening_queue.put(
                     (1, Packets(
                         content={
@@ -30,8 +33,12 @@ def main(text:str, queues):
                         }, queue=listening_queue
                     ))
                 )
-                return 1
-            
-            logger.info("Playing music....")
+
+                event.clear()
+                isRunning = False
+                return "ending"
+            else:
+                logger.info("Playing music....")
+
         except Exception as e:
             logger.critical(f"MUSIC: ERROR: {e}")
